e2bd9165c36351c4af052e8ef55904a1726907e6
Guille Mesyngier <gmesyngier@gmail.com>
2016-02-17 09:52:31 -0300

    project functionality

---
+++ components/masotros/projects.sizes.js
@@ -0,0 +1,47 @@
+// Carrousell
+
+$(window).load(function(){
+	$('#carousel').flexslider({
+		animation: "slide",
+		controlNav: false,
+		directionNav: true,
+		animationLoop: true,
+		slideshow: false,
+		itemWidth: 210,
+		touch: true, 
+		itemMargin: 0,
+		asNavFor: '#slider',
+	});
+
+	$('#slider').flexslider({
+		animation: "slide",
+		controlNav: false,
+		directionNav: false,
+		animationLoop: true,
+		slideshow: false,
+		touch: true, 
+		sync: "#carousel",
+		start: function(slider){
+			$('body').removeClass('loading');
+		}
+	});
+});
+
+$(window).load(function(){
+	resizeProyectImage();
+});
+
+$(window).on('resize', function(){
+	resizeProyectImage();
+});
+
+function resizeProyectImage() {
+	if(window.innerHeight >= $('.screen li img').innerHeight())
+	{
+		$('.screen li img').css({'width':'auto', 'height': '100vh'});
+	}
+	if(window.innerWidth >= $('.screen li img').innerWidth()) {
+		$('.screen li img').css({'width':'100vw', 'height': 'auto'});
+	} 
+};
+
